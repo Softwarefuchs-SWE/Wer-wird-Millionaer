9d333fd397b98a70b4f4a09b226d4fff0d20ccfd
Hundi <48763207+HundiDerEchte@users.noreply.github.com>
2023-11-29 23:28:28 +0100

    andere header erstellt und Änderungen an basics.css

---
+++ view/templates/full_header.php
@@ -0,0 +1,25 @@
+<link href="/Stylesheets/header.css" rel="stylesheet">
+
+<header class="header_grid-container">
+    <div class="header_item_name">
+        <label id="nutzername" class="fontsize-20px">Testnutzername (25x&#9733;)</label>
+    </div>
+
+    <div class="header_item_knöpfe align-content-right">
+        <button class="knopfT3Rund shadow-outset" type="button">
+            <img src="/Ressources/Images/Ton%20Button.png" alt="Ton ändern" width="60" height="60">
+        </button>
+
+        <button class="knopfT3Rund shadow-outset" type="button">
+            <img src="/Ressources/Images/Hilfe%20Button.png" alt="Hilfe anzeigen" width="60" height="60">
+        </button>
+    </div>
+
+    <div class="header_item_logo align-content-center">
+        <img id="WWM-Logo" src="/Ressources/Images/Logo.png" alt="logo" width="185" height="150">
+    </div>
+
+    <div class="header_item_fhlogo align-content-right shadow-outset">
+        <img id="fh-logo" src="/Ressources/Images/FH-Logo.png"  alt="FH_Logo" width="60" height="150">
+    </div>
+</header>
