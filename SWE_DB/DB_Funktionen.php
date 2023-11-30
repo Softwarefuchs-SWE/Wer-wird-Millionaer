8d9063d61d1a54f96fa1465c60c89b53814f83b6
jul0037 <119369348+jul0037@users.noreply.github.com>
2023-11-30 17:43:32 +0100

    Update DB_Funktionen.php
    bin lost

--- SWE_DB/DB_Funktionen.php
+++ SWE_DB/DB_Funktionen.php
@@ -1,1 +1,45 @@
+<?php
 
+function connect_to_db()
+{
+  $link = mysqli_connect("localhost", "root", "123", "swe_db");
+
+  if (!$link)
+  {
+    echo "Connection to swe_db failed: " . mysqli_connect_error();
+    exit();
+  }
+
+  mysqli_set_charset($link, "utf8");
+
+  return $link;
+}
+
+
+$link = connect_to_db();
+
+$sql = "SELECT Frage, Antwort_1, Antwort_2, Antwort_3, Antwort_4
+        FROM Fragen
+        WHERE Frage_schwierigkeit = 1
+        LIMIT 3";
+
+//$result = mysqli_fetch_assoc(mysqli_query($link, $sql));
+
+$result1 = mysqli_query($link, $sql);
+
+if (!$result1)
+{
+  echo "Fehler während der Abfrage:  ", mysqli_error($link);
+  exit();
+}
+
+while ($row = mysqli_fetch_assoc($result1))
+{
+  foreach ($row as $value)
+  {
+    echo "<td>" . $value . "</td>" . "<br>";
+  }
+  echo "</tr>" . "<br>";
+}
+
+mysqli_close($link);
