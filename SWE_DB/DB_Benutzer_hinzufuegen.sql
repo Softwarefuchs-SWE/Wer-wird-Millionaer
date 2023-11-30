e2f0bcb6b5f151c2ffe87f6753cf276ff1c5e70a
jul0037 <119369348+jul0037@users.noreply.github.com>
2023-11-30 17:57:52 +0100

    Add files via upload

---
+++ SWE_DB/DB_Benutzer_hinzufuegen.sql
@@ -0,0 +1,9 @@
+USE swe_db;
+
+INSERT INTO Benutzerdaten (Vorname, Nachname, Geburtstag, Name_erstes_Haustier, Nachname_Mutter, Lieblingszahl, Passwort)
+VALUES
+  ('Max', 'Mustermann', '1990-05-15', 'Bello', 'Mustermann', 7, 'passwort123'),
+  ('Anna', 'Schmidt', '1985-09-22', 'Whiskers', 'Schmidt', 5, 'geheim123'),
+  ('Lena', 'Müller', '1998-11-30', 'Fluffy', 'Müller', 3, '12345pass'),
+  ('Tom', 'Meier', '1982-07-10', 'Rex', 'Meier', 9, 'sicher456'),
+  ('Sophie', 'Wagner', '2000-03-25', 'Mittens', 'Wagner', 8, 'pass123wort');
