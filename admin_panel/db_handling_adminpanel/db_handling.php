d9d6e1bad8dc51e894b74789f0401e40e1e475bd
herr-dennis <herr.dennisblacky@hotmail.de>
2023-12-04 13:49:10 +0100

    Frage ändern Logik Fehler behoben und ergänzt.

--- admin_panel/db_handling_adminpanel/db_handling.php
+++ admin_panel/db_handling_adminpanel/db_handling.php
@@ -1,6 +1,8 @@
 <?php
 
-
+/**
+Funktion die eine Verbindung zur Datenbank aufbaut
+ */
 function connect_to_db()
 {
     $link = mysqli_connect("localhost", "root", "ihesp", "swe_db");
@@ -16,23 +18,32 @@ function connect_to_db()
     return $link;
 }
 
-
+/**
+Funktion die nur den Fragentext aus der Datenbank holt
+ */
 function get_question_label() : array {
 
     $link = connect_to_db();
     $sql = "SELECT Frage FROM fragen";
 
     $result = mysqli_query($link,$sql);
+    if(!$result){
+        die('Fehler in der Abfrage: ' . mysqli_error($link));
+    }
+
     $fragen=[];
+
     while ( $row =mysqli_fetch_assoc($result)){
 
         $fragen[] = $row;
     }
-    var_dump($fragen);
+
     mysqli_close($link);
     return $fragen;
 }
-
+/**
+Funktion die alle Fragen auf der Datenbank zurückgibt
+ */
 function get_question_full(){
 
 
@@ -40,14 +51,43 @@ function get_question_full(){
     $sql = "SELECT * FROM fragen";
 
     $result = mysqli_query($link,$sql);
+
+    if(!$result){
+        die('Fehler in der Abfrage: ' . mysqli_error($link));
+    }
+
     $fragen=[];
     while ( $row =mysqli_fetch_assoc($result)){
 
         $fragen[] = $row;
     }
-    var_dump($fragen);
     mysqli_close($link);
     return $fragen;
 
 
+}
+/**
+ Datenbankfunktion die über die ID (Primary-Key) die Fragen
+ * zurückgibt.
+ */
+function get_question_by_id( $id) : array{
+
+    //Verbindung zur Datenbank
+    $link = connect_to_db();
+    //Verhinderung von SQL-Injection
+    $id = mysqli_real_escape_string($link, $id);
+
+    $sql = "SELECT * FROM fragen WHERE id='$id'";
+
+    $result = mysqli_query($link,$sql);
+    if (!$result) {
+        die('Fehler in der Abfrage: ' . mysqli_error($link));
+    }
+    $frage= [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $frage[]= $row;
+    }
+
+    mysqli_close($link);
+    return $frage;
 }
