2140abf3453da0aced1f7d6d90229f9855dbee81
Hundi <48763207+HundiDerEchte@users.noreply.github.com>
2023-12-06 20:14:52 +0100

    Music button logik dreckig+

--- Beispiel_Anwendung_Header.php
+++ Beispiel_Anwendung_Header.php
@@ -2,11 +2,42 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Wer wird Millionär</title>
+
     <link href="admin_panel/view/inputTemplate.css" rel="stylesheet">
     <link href="admin_panel/view/basics.css" rel="stylesheet">
     <link href="admin_panel/view/header.css" rel="stylesheet">
 </head>
+
+<script>
+    function changeMusik() {
+        var audio1 = document.getElementById("audio1");
+        var audio2 = document.getElementById("audio2");
+
+        if (audio1.paused && audio2.paused){
+            audio1.play();
+        }
+        else if (!audio1.paused && audio2.paused){
+            audio1.pause();
+            audio2.play();
+        }
+        else
+        {
+            audio1.pause();
+            audio2.pause();
+        }
+    }
+
+    function play2() {
+        var audio = document.getElementById("audio2");
+        audio.play();
+        var audio = document.getElementById("audio1");
+        audio.pause();
+    }
+</script>
+
 <body>
 
 <?php include "Ressources/templates/header/full_header.php" ?>
@@ -15,7 +46,11 @@
     <form class="align-content-center" action="index.html" method="get">
         <input type="text" class="inputField">
     </form>
-</main>
 
+    <audio id="audio1" loop src="Ressources\music\music_lofi.mp3"></audio>
+    <audio id="audio2" loop src="Ressources\music\music_dark.mp3"></audio>
+
+    <button onclick="changeMusik()">Musik ändern </button>
+</main>
 </body>
 </html>
