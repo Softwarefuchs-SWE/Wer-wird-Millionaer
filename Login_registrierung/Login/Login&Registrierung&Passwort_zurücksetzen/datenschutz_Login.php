039a0d4ff8b0229406239bd13333f257256268f8
Hundi <48763207+HundiDerEchte@users.noreply.github.com>
2024-01-22 14:35:04 +0100

    Impressumheader und Datenschutzheader repariert

--- Login_registrierung/Login/Login&Registrierung&Passwort_zurücksetzen/datenschutz_Login.php
+++ Login_registrierung/Login/Login&Registrierung&Passwort_zurücksetzen/datenschutz_Login.php
@@ -7,19 +7,17 @@ session_start();
     <title>Datenschutz</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  type="text/css" href="../Stylesheets/basics.css">
-    <link rel="stylesheet"  type="text/css" href="../Stylesheets/inputTemplate.css">
+    <link rel="stylesheet"  type="text/css" href="../../../Stylesheets/basics.css">
+    <link rel="stylesheet"  type="text/css" href="../../../Stylesheets/inputTemplate.css">
     <link rel="stylesheet" type="text/css"  href="../../../Stylesheets/hauptmenu.css">
+    <link rel="stylesheet" type="text/css"  href="../../../admin_panel/view/header.css">
     <script src="../Ressources/audioManager.js"></script>
     <script src="login.js"></script>
 </head>
 <body>
-<header class="header_grid-container">
 
-    <div class="header_item_logo align-content-center">
-        <h1 id="headline">Datenschutz</h1>
-    </div>
-</header>
+<?php include ("../../../Ressources/templates/header/lite_header.php")?>
+
 <div class="main_container">
     <h1>Datenschutz</h1>
     <p>Es gibt keinen Datenschutz. Anmeldung auf eigene Gefahr!</p>
--- Login_registrierung/Login/Login&Registrierung&Passwort_zurücksetzen/impressum_Login.php
+++ Login_registrierung/Login/Login&Registrierung&Passwort_zurücksetzen/impressum_Login.php
@@ -7,19 +7,17 @@ session_start();
     <title>Impressum</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  type="text/css" href="../Stylesheets/basics.css">
-    <link rel="stylesheet"  type="text/css" href="../Stylesheets/inputTemplate.css">
+    <link rel="stylesheet"  type="text/css" href="../../../Stylesheets/basics.css">
+    <link rel="stylesheet"  type="text/css" href="../../../Stylesheets/inputTemplate.css">
     <link rel="stylesheet" type="text/css"  href="../../../Stylesheets/hauptmenu.css">
+    <link rel="stylesheet" type="text/css"  href="../../../admin_panel/view/header.css">
     <script src="../Ressources/audioManager.js"></script>
     <script src="login.js"></script>
 </head>
 <body>
-<header class="header_grid-container">
 
-    <div class="header_item_logo align-content-center">
-        <h1 id="headline">Impressum</h1>
-    </div>
-</header>
+<?php include ("../../../Ressources/templates/header/lite_header.php")?>
+
 <div class="main_container">
     <h1>Impressum</h1>
     <h2>Angaben gemäß § 5 TMG:</h2>
